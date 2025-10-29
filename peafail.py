70aa0d42beaf07200c76c65a582d82f83be877df
Sandra Karo <sandrakaro@Sandras-MacBook-Air.local>
2025-10-29 02:25:49 +0200

    esimese faili loomine

---
+++ peafail.py
@@ -0,0 +1,14 @@
+# Siin on meie peamine fail
+# Siin on kirjeldus
+# Siin on autorid
+
+# Siin on deepface githubi repo https://github.com/serengil/deepface
+# Saab lisada custom emotsioone
+
+from deepface import DeepFace
+
+def main():
+    placeholder = 1
+
+if __name__ == main():
+    main()
