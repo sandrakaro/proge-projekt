0d1f91d769bd1f75e11cfa22416f955643f4dea4
Sandra Karo <sandrakaro@Sandras-MacBook-Air.local>
2025-10-30 17:52:49 +0200

    peafaili esimene URL

--- peafail.py
+++ peafail.py
@@ -1,14 +1,24 @@
 # Siin on meie peamine fail
 # Siin on kirjeldus
 # Siin on autorid
+# vms siin olema peab
 
-# Siin on deepface githubi repo https://github.com/serengil/deepface
-# Saab lisada custom emotsioone
+import requests
+from bs4 import BeautifulSoup
 
-from deepface import DeepFace
-
-def main():
-    placeholder = 1
-
-if __name__ == main():
-    main()
+lauaViinURL = 'https://www.selver.ee/laua-viin-liviko-50-cl'
+# HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+try:
+    päring = requests.get(lauaViinURL)
+    if päring.status_code == 200:
+        soup = BeautifulSoup(päring.content, 'html.parser')
+        hind = soup.find('div',klass='ProductPrice')
+        if hind:
+            hind = hind.text.strip()
+            print(f'Praegune hind on {hind}')
+        else:
+            print('Hinda ei leitud, kontrolli klassi nime')
+    else:
+        print('Päring ebaõnnestus')
+except requests.exceptions.RequestException as viga:
+    print(f'Päringu viga {viga}')
