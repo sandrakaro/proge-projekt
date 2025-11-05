88c37a09575843fca815f6638206c145f3ed5bbd
Sandra Karo <sandrakaro@Sandras-MacBook-Air.local>
2025-11-06 00:18:56 +0200

    parandada koodi peafailis, lisada linkide txt

--- peafail.py
+++ peafail.py
@@ -3,23 +3,47 @@
 # Siin on autorid
 # vms siin olema peab
 
-import requests
-from bs4 import BeautifulSoup
+# Praegu on programm selline, et kasutaja sisestab joogi nime ja saab ainult selle joogi hinna
+# Kui requestida kõiki URL-e korraga, siis Selver ei lase päringuid läbi
+# Ja nende selver.ee/robots.txt ütleb disallow price= seegaaa, ma ei tea millal nad mu IP ära võivad bännida
+# Pm võib nt Prismaga ka proovida, aga võivad samad probleemid olla
+# Also see ühe URL-i requestimine võtab väga kaua aega, wait 10 s soovitas Gemini
+# seega ma ei tea kas asi töötab, kui seda lühemaks panna
+# Peaks välja mõtlema, mis see lõpplahendus olla võiks
 
-lauaViinURL = 'https://www.selver.ee/laua-viin-liviko-50-cl'
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+joogiFail = open('jookide-lingid.txt',encoding='utf-8')
+joogiJärjend = [el.strip().split(';') for el in joogiFail.readlines()]
+joogiFail.close()
+joogiSõnastik = {el[0]: el[1] for el in joogiJärjend if '---' not in el[0]} # loome sõnastiku, kust saab otsida jookide linke joogi nime järgi; ei arvesta kolme sidekriipsuga tootekategooriaid
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless') # ei ava reaalset brauseri akent
+driver = webdriver.Chrome(options=options) # kasutab brauserina Google Chrome'i
+### hinnad = set()
+
+joogiSoov = input('Sisesta joogi nimi: ')
+joogiSooviLink = joogiSõnastik[joogiSoov]
+
 try:
-    päring = requests.get(lauaViinURL, headers=HEADERS)
-    if päring.status_code == 200:
-        soup = BeautifulSoup(päring.content, 'html.parser')
-        hind = soup.find('div',class_='ProductPrice#text')
-        hind = soup.select_one('div.ProductPrice')
-        if hind:
-            hind = hind.text.strip()
-            print(f'Praegune hind on {hind}')
-        else:
-            print('Hinda ei leitud, kontrolli klassi nime')
+    driver.get(joogiSooviLink) # avab soovitud joogi lingi
+    wait = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'ProductPrice'))) # ootab kuni JavaScripti hind on lehel laetud
+    päringuSisu = driver.page_source
+    soup = BeautifulSoup(päringuSisu, 'html.parser')
+    hinnaElement = soup.find('div', class_='ProductPrice')
+    if hinnaElement:
+        hind = hinnaElement.text.strip().split('€')[0] # ei anna liitrihinda ega euromärki
+        hind = float(hind.replace(',','.')) # teeme komaga hinna ujukomaarvuks
+        print(f'Selle joogi praegune hind on {hind} €')
     else:
-        print('Päring ebaõnnestus')
-except requests.exceptions.RequestException as viga:
+        print('Hinda ei leitud, kontrolli klassi nime')
+except Exception as viga:
     print(f'Päringu viga {viga}')
+finally:
+    driver.quit()
