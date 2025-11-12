91e847f412d8943162435edb984bda11f67ab538
sandrakaro <sandra.karo.2006@gmail.com>
2025-11-12 12:07:26 +0200

    Update peafail.py

--- peafail.py
+++ peafail.py
@@ -4,7 +4,7 @@
 # ---Muu oluline info: 
 # ---Edasised plaanid: lisada GUI Tkinteriga, selleks ka kraapida koos tootega tema pilt, lisada kasutajalt küsimine, kas teeb
 # shotte, kokteili või mõlemat, lisada selleks vajalikud funktsionaalsused, nt shotisõnastik,
-# korrastada koodi, optimiseerida, teha loetavamaks, lisada kommentaare
+# korrastada koodi, optimiseerida, teha loetavamaks, lisada kommentaare, täita vormistusnõuded
 
 # Prisma robots.txt lubab kõike kraapida
 # Ühe URL-i requestimine võtab veits kaua aega, wait 10 s soovitas Gemini
